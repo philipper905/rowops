073f07ed9c7b723effb5257e1c2da2d1994a013a
Philippe R <philipper905@gmail.com>
2020-03-27 12:48:12 -0700

    Define Opt struct containing command line options and defaults

--- src/opt.rs
+++ src/opt.rs
@@ -1,2 +1,18 @@
-use std::path::PathBuf;
 use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rowops", about = "easily manipulated matrices visually")]
+
+pub struct Opt {
+    #[structopt(short = "c", default_value = "4")]
+    cols: u8,
+
+    #[structopt(short = "r", default_value = "3")]
+    rows: u8,
+}
+
+impl Opt {
+    pub fn create() -> Self {
+        Opt::from_args()
+    }
+}
