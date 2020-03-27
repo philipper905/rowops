d0044b6c8aaeaa28f6edcadba2d7b0855bb9d74e
Philippe R <philipper905@gmail.com>
2020-03-27 13:17:52 -0700

    Add MatrixBuilder struct

--- src/matrix.rs
+++ src/matrix.rs
@@ -6,11 +6,47 @@ pub struct Matrix {
 }
 
 impl Matrix {
-    fn new(rows: u8, cols: u8) -> Matrix{
+    pub fn new(rows: u8, cols: u8) -> Matrix{
         Matrix {
             rows,
             cols,
             inner: vec![0.0; (rows * cols) as usize],
         } 
+    }
+}
+
+#[derive(Default)]
+pub struct MatrixBuilder {
+    rows: u8,
+    cols: u8,
+}
+
+impl MatrixBuilder {
+    pub fn new() -> MatrixBuilder {
+        MatrixBuilder::default()
+    }
+
+    pub fn rows(mut self, rows: u8) -> MatrixBuilder {
+        self.rows = rows;
+        return self
+    }
+
+    pub fn columns(mut self, columns: u8) -> MatrixBuilder {
+        self.cols = columns;
+        return self
+    }
+
+    pub fn cols(self, cols: u8) -> MatrixBuilder {
+        self.columns(cols)
+    }
+
+    pub fn build(self) -> Matrix {
+        Matrix::new(self.rows, self.cols)
+    }
+}
+
+impl From<MatrixBuilder> for Matrix {
+    fn from(x: MatrixBuilder) -> Matrix {
+        x.build()
     }
 }
